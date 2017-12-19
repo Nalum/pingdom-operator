8411d47d79f87d86654a4a5534504ce2b3b70c66
Luke Mallon (Nalum) <luke@mallon.io>
2017-12-19 19:32:20 +0000

    More tests

--- pingdom/httpcheck_test.go
+++ pingdom/httpcheck_test.go
@@ -6,12 +6,66 @@ func TestNewHTTPCheck(t *testing.T) {
 	check, err := NewHTTPCheck("testing", "https://this.is/a/test")
 
 	if err != nil {
-		t.Fail()
 		t.Error(err)
 	}
 
 	if check.GetType() != CheckTypeHTTP {
 		t.Fail()
 		t.Log("Failed checking the Type of the Check")
+	}
+}
+
+func TestNewHTTPCheckWithAuth(t *testing.T) {
+	check, err := NewHTTPCheck("testing", "https://jim:car@this.is/a/test")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if check.GetType() != CheckTypeHTTP {
+		t.Fail()
+		t.Log("Failed checking the Type of the Check")
+	}
+}
+
+func TestNewHTTPCheckWithPort(t *testing.T) {
+	check, err := NewHTTPCheck("testing", "https://this.is:8080/a/test")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if check.GetType() != CheckTypeHTTP {
+		t.Fail()
+		t.Log("Failed checking the Type of the Check")
+	}
+}
+
+func TestHTTPCheckSetData(t *testing.T) {
+	check, _ := NewHTTPCheck("testing", "https://this.is/a/test")
+
+	err := check.SetData(map[string]interface{}{})
+
+	if err != nil {
+		t.Fail()
+		t.Log(err)
+	}
+}
+
+func TestHTTPCheckBadPort(t *testing.T) {
+	_, err := NewHTTPCheck("testing", "https://this:99999999.is/a/test")
+
+	if err == nil {
+		t.Fail()
+		t.Log("Error Expected but none given")
+	}
+}
+
+func TestHTTPCheckBadURL(t *testing.T) {
+	_, err := NewHTTPCheck("testing", "-https://this:99999999.is/a/test")
+
+	if err == nil {
+		t.Fail()
+		t.Log("Error Expected but none given")
 	}
 }
